13ee9842290b9c4faa5767c4580dba174fcb6784
fedekau <fede.kau@gmail.com>
2016-06-04 15:25:22 -0700

    Created laboratorio3 package and launch file with husky robot

---
+++ build/laboratorio3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/federico/catkin_ws/src/laboratorio3
+# Build directory: /home/federico/catkin_ws/build/laboratorio3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
