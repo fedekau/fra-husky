d95f38eabda54dacee539af9bccbbec1888a9d9f
fedekau <fede.kau@gmail.com>
2016-06-17 14:17:38 -0700

    Remove unnecesary code

--- src/laboratorio3/scripts/move_forward.py
+++ src/laboratorio3/scripts/move_forward.py
@@ -3,17 +3,6 @@ import rospy
 from explorer import Explorer
 from std_msgs.msg import String
 
-def talker():
-	pub = rospy.Publisher('/cmd_vel', Twist , queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(10) # 10hz
-	
-	while not rospy.is_shutdown():
-		twist = Twist(Vector3(0,0,0),Vector3(0,0,0))
-		pub.publish(twist)
-		rate.sleep()
-
-
 if __name__ == '__main__':
 	try:
 		rospy.init_node('move_forward', anonymous = False)
